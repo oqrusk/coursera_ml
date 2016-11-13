837179b534dbf1a6a5a92bb56f70ee228c303b67
oqrusk <oqrusk@gmail.com>
2016-11-13 11:15:09 +0900

    Compute Centroid Means

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%COMPUTECENTROIDS returs the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -25,16 +25,19 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
+for k=1: K,
+  j = 1;
+  Z = [];
+  for i=1: m,
+    if idx(i,1) == k,
+      Z(j,:) = X(i,:);
+      j = j + 1;
+    end
+  end
+  centroids(k,:) = sum(Z) ./ (j-1);
+end
 
 % =============================================================
 
 
 end
-
